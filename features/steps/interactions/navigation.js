cdc5f916458371eab03070f9713070559c957811
Juho Härme <juho.harme@gmail.com>
2019-03-18 09:23:10 +0200

    fix urls in tests

--- features/steps/interactions/navigation.js
+++ features/steps/interactions/navigation.js
@@ -4,7 +4,7 @@ import "babel-polyfill"; // NOTE: needed for aync await
 import assert from "assert";
 
 const base_url = `http://${process.env.FRONTEND_HOST}:${
-  process.env.FRONTEND_HOST
+  process.env.FRONTEND_PORT
 }`;
 
 When(/the user navigates to ([^s]+)/, function(location) {
