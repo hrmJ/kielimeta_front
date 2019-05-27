c097b1df8686013fd37411fe79ef43d626cef5ac
Juho Härme <juho.harme@utu.fi>
2019-05-27 11:09:24 +0300

    Add delay to ds serch field
    In order to prevent the asynchronous mess sometimes
    occuring when typing fast and the result for
    a filter with fewer letters comes in last

--- src/components/content/datasetlist/index.jsx
+++ src/components/content/datasetlist/index.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
 import { listAll, filterByQuery } from '../../../redux/actions/datasets';
-import Filter from '../filter';
-import DatasetItem from '../datasetitem';
-import styles from './datasetlist.scss';
 import { printLanguageName } from '../languagebadge';
+import DatasetItem from '../datasetitem';
+import DelayedSearchField from '../../ui/DelayedSearchField';
+import Filter from '../filter';
 import OrderSelect from '../../ui/orderselect';
+import styles from './datasetlist.scss';
 
 export default class DatasetList extends Component {
   static get propTypes() {
     return {
       dispatch: PropTypes.func.isRequired,
-      datasets: PropTypes.arrayOf(PropTypes.object).isRequired
+      datasets: PropTypes.arrayOf(PropTypes.object).isRequired,
     };
   }
 
@@ -23,20 +25,22 @@ export default class DatasetList extends Component {
   }
 
   filterDatasets(query) {
+    console.debug(query);
     const { dispatch, filters } = this.props;
-    dispatch(filterByQuery({ ...filters, query: query }));
+    dispatch(filterByQuery({ ...filters, query }));
   }
 
   render() {
-    const { datasets, dispatch, filters, originalFilterValues } = this.props;
+    const {
+      datasets, dispatch, filters, originalFilterValues,
+    } = this.props;
 
     return (
       <div id="resources" className={styles.datasetlistContainer}>
         <section className={styles.searchBarContainer}>
-          <input
+          <DelayedSearchField
             id="searchfield"
-            type="text"
-            onChange={ev => this.filterDatasets(ev.target.value)}
+            onChange={this.filterDatasets.bind(this)}
             placeholder="Hae nimellä tai avainsanalla"
           />
         </section>
@@ -63,7 +67,10 @@ export default class DatasetList extends Component {
           <Filter>Lisää suodattimia </Filter>
         </section>
         <section className={styles.resultNumberContainer}>
-          <div>Aineistoja näillä suodattimilla: {datasets.length}</div>
+          <div>
+            Aineistoja näillä suodattimilla:
+            {datasets.length}
+          </div>
           <div>
             <OrderSelect items={['nimi', 'koko']} />
           </div>
