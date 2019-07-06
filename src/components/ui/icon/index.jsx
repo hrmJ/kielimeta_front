ce6b5d840ab066a19b97f306634fe45d5cd2c939
Juho Härme <juho.harme@utu.fi>
2019-07-06 17:58:52 +0300

    Hacky solution to dynamically loading svg icons

--- src/components/ui/icon/index.jsx
+++ src/components/ui/icon/index.jsx
@@ -3,19 +3,31 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Icon extends Component {
-  state = { pickedIcon: null };
+  state = { test: false };
 
-  componentDidMount = async () => {
-    const { iconName } = this.props;
-    if (iconName) {
-      const svgIcons = await import('@fortawesome/free-solid-svg-icons');
-      this.setState({ pickedIcon: svgIcons[iconName] });
+  svgIcons = null;
+
+  isMountedHack = false;
+
+  componentDidMount() {
+    this.isMountedHack = true;
+    if (this.isMountedHack) {
+      this.getIcons();
     }
-  };
+  }
+
+  componentWillUnmount() {
+    this.isMountedHack = false;
+  }
+
+  async getIcons() {
+    this.svgIcons = await import(/* webpackPrefetch: true */ `@fortawesome/free-solid-svg-icons`);
+    this.setState({ test: true });
+  }
 
   render() {
-    const { role, onClick, className } = this.props;
-    const { pickedIcon } = this.state;
+    const { role, onClick, className, iconName } = this.props;
+    const pickedIcon = this.svgIcons && this.svgIcons[iconName];
     const otherProps = {};
     if (role) {
       otherProps.role = role;
