7f81f39edb9a064f844c064d1665a1ffa936d754
Juho Härme <juho.harme@utu.fi>
2019-07-15 17:04:11 +0300

    another take on target language representation

--- src/components/content/datasetitem/languageContainer/translationContainer.jsx
+++ src/components/content/datasetitem/languageContainer/translationContainer.jsx
@@ -1,8 +1,9 @@
-import PropTypes from 'prop-types';
 import { uid } from 'react-uid';
-import React, { Component } from 'react';
 import { wrapGrid } from 'animate-css-grid';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
+import LanguageContainer from '.';
 import LanguageDetails from '../languageDetails';
 import styles from './languageContainer.scss';
 
@@ -14,22 +15,18 @@ class TranslationContainer extends Component {
   render() {
     const { sl, tl } = this.props;
     return (
-      <div className={styles.translationContainer} ref={el => (this.grid = el)}>
-        <LanguageDetails key={uid(sl)} {...sl} additionalClassname={styles.slContainer} />
-        <h5>
-          <div className={styles.arrow_box}>
-            <div className={styles.tlHeading}>
-              <div>Kohdekielet</div>
+      <div>
+        <div className={styles.translationContainer} ref={el => (this.grid = el)}>
+          <LanguageDetails key={uid(sl)} {...sl} additionalClassname={styles.slContainer} />
+          <h5>
+            <div className={styles.arrow_box}>
+              <div className={styles.tlHeading}>
+                <div>Kohdekielet</div>
+              </div>
             </div>
-          </div>
-        </h5>
-        {tl.map(language => (
-          <LanguageDetails
-            key={uid(language)}
-            {...language}
-            additionalClassname={styles[`tlContainer${tl.length < 4 && tl.length}`]}
-          />
-        ))}
+          </h5>
+        </div>
+        <LanguageContainer languages={tl} />
       </div>
     );
   }
