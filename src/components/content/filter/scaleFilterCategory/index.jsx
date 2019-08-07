f2443f391449ce6767b9e8b8d19c752b093b934d
Juho Härme <juho.harme@utu.fi>
2019-08-07 14:21:22 +0300

    trying to make scale filters snappier

--- src/components/content/filter/scaleFilterCategory/index.jsx
+++ src/components/content/filter/scaleFilterCategory/index.jsx
@@ -1,32 +1,39 @@
 import InputRange from 'react-input-range';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
 import { updateAndFilter } from '../../../../redux/actions/filters';
 import styles from './scalefiltercategory.scss';
 import rangeStyles from '../../../../vendorstyles/unmodifiedSass_inputRange/input-range.scss';
 
-const scaleFilterCategory = props => {
-  const { min, max, label, filterKey, dispatch, filters } = props;
-  let value;
-  if (filterKey in filters) {
-    value = { min: filters[filterKey][0], max: filters[filterKey][1] };
+class scaleFilterCategory extends Component {
+  state = { value: undefined };
+
+  updateValue(val) {
+    const { dispatch, filters, filterKey } = this.props;
+    this.setState({ value: val });
+    dispatch(updateAndFilter(filterKey, [val.min, val.max], true, filters));
   }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.label}>{label}</div>
-      <div>
-        <InputRange
-          maxValue={max}
-          minValue={min}
-          onChange={val => dispatch(updateAndFilter(filterKey, [val.min, val.max], true, filters))}
-          value={value || { min, max }}
-        />
+  render() {
+    const { min, max, label } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.label}>{label}</div>
+        <div>
+          <InputRange
+            maxValue={max}
+            minValue={min}
+            onChange={val => this.updateValue(val)}
+            value={value || { min, max }}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 scaleFilterCategory.propTypes = {
   min: PropTypes.number.isRequired,
